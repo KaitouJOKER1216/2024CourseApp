5157ed38016c14f14ed3f1f73d48d429610481c4
喬克Kaitou JOKER <sonic7061@gmail.com>
2024-11-26 11:01:21 +0800

    新增 Student 和 Teacher 類別及相關檔案
    在 `2024CourseApp.vcxproj` 中，修正了 XML 宣告的格式，並新增了 `Student.cpp` 和 `Teacher.cpp` 到 `ClCompile` 區塊，新增了 `Student.h`、`Teacher.h` 和 `Utility.h` 到 `ClInclude` 區塊。
    
    在 `2024CourseApp.vcxproj.filters` 中，新增了 `Student.cpp` 和 `Teacher.cpp` 到 `ClCompile` 區塊，並新增了 `Student.h`、`Teacher.h` 和 `Utility.h` 到 `ClInclude` 區塊。
    
    在 `Student.cpp` 中，新增了 `#include "Student.h"`。
    
    在 `Student.h` 中，新增了 `#pragma once`，並包含了 `Person.h` 和 `Utility.h`，定義了一個繼承自 `Person` 的 `Student` 類別，並新增了 `studentId`、`department` 和 `className` 成員變數。
    
    在 `Teacher.cpp` 中，新增了 `#include "Teacher.h"`。
    
    在 `Teacher.h` 中，新增了 `#pragma once`，並包含了 `Person.h`，定義了一個繼承自 `Person` 的 `Teacher` 類別。
    
    在 `Utility.h` 中，新增了 `#pragma once`，並定義了 `Department` 和 `ClassName` 枚舉類型。

---
+++ 2024CourseApp/Utility.h
@@ -0,0 +1,19 @@
+#pragma once
+using namespace std;
+
+enum class Department {
+	ComputerScience,
+	ElectricalEngineering,
+	ElectronicEngineering,
+};
+
+enum class ClassName {
+	_1A,
+	_1B,
+	_2A,
+	_2B,
+	_3A,
+	_3B,
+	_4A,
+	_4B,
+};
